1ae56c24b7a63d75389ac6120016ba2454a8d59f
Oscar Lin <linoscar01@gmail.com>
2022-12-16 15:36:57 -0500

    home page content

--- pages/home.js
+++ pages/home.js
@@ -8,7 +8,7 @@ import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRound
 
 const Home = () => {
   return (
-    <div>
+    <div className="space-y-10 relative">
       <header className="flex justify-around items-center py-4">
         <div className="relative w-36 h-10">
           <Image src={"/LinkedIn_Logo.svg"} fill style={"contain"} />
@@ -27,6 +27,32 @@ const Home = () => {
           </div>
         </div>
       </header>
+
+      <main className="flex flex-col xl:flex-row items-center max-w-screen-lg mx-auto">
+        <div className="space-y-6 xl:space-y-10">
+          <h1 className="text-3xl md:text-5xl  text-amber-800/80 max-w-xl !leading-snug pl-4 xl:p-0">
+            Welcome to Your Professional Community
+          </h1>
+          <div className="space-y-4">
+            <div className="intent">
+              <h2 className="text-xl">Search for a job</h2>
+              <ArrowForwardIosRoundedIcon className="text-gray-700" />
+            </div>
+            <div className="intent">
+              <h2 className="text-xl">Find a person you know</h2>
+              <ArrowForwardIosRoundedIcon className="text-gray-700" />
+            </div>
+            <div className="intent">
+              <h2 className="text-xl">Learn a new skill</h2>
+              <ArrowForwardIosRoundedIcon className="text-gray-700" />
+            </div>
+          </div>
+        </div>
+
+        <div className="relative xl:absolute w-80 h-80 xl:w-[650px] xl:h-[650px] top-14 right-5">
+          <Image src={"/linkedin_pic1.svg"} fill style={'contain'} priority/>
+        </div>
+      </main>
     </div>
   );
 }
