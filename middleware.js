29e7161689a07324cea92ed4a60da15f52594521
Oscar Lin <linoscar01@gmail.com>
2022-12-22 13:01:31 -0500

    updating middleware

--- middleware.js
+++ middleware.js
@@ -5,13 +5,15 @@ import { getToken } from 'next-auth/jwt'
 // This function can be marked `async` if using `await` inside
 export async function middleware(request) {
 
-  if(request.nextUrl.pathname === '/' ){
+  const {pathname, origin} = request.nextUrl;
+
+  if(pathname === '/' ){
     const session = await getToken({
       req: request,
       secret: process.env.JWT_SECRET,
       secureCookie: process.env.NODE_ENV === "production",
     });
-    if(!session) return NextResponse.redirect(new URL('/home', request.url));
+    if(!session) return NextResponse.redirect(`${origin}/home`);
   }
 
 
